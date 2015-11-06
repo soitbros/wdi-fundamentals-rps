7da08e5056c3801059a7cd8e13e588e2004e49b6
Clint Osterholz <clintosterholz@Clints-MacBook-Air-2.local>
2015-11-06 14:16:31 -0500

    Unit 4 Homework

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,23 +22,41 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-	move === true;
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return move /* Your Expression */;
+    return move || getInput();
 }
 
 function getComputerMove(move) {
-	move === true;
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return move /* Your Expression */;
+    return move || randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
     var winner;
+    if (playerMove === "rock") {
+    	if (computerMove === "rock") {
+    	winner = "tie";
+    	} else if (computerMove === "paper") {
+    	winner = "computer";
+    	} else if (computerMove === "scissors") {
+    	winner = "player";
+    	}
+    } else if (playerMove === "scissors") {
+    	if (computerMove === "rock") {
+    	winner = "computer";
+    	} else if (computerMove === "paper") {
+    	winner = "player";
+    	} else if (computerMove === "scissors") {
+    	winner = "tie";
+    	}
+    } else {
+        if (computerMove === "rock") {
+    	winner = "player";
+    	} else if (computerMove === "paper") {
+    	winner = "tie";
+    	} else if (computerMove === "scissors") {
+    	winner = "computer";
+    	}
+    };
+};
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
